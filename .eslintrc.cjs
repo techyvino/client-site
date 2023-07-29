f32fecd5532d1531c370975b367a01f9573d2360
Vinoth <techyvino@gmail.com>
2023-07-29 23:30:26 +0530

    integrated tailwind and folder structure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,24 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '*.config.js'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css';
+import { Button } from '@nextui-org/react';
 
 function App() {
-
   return (
     <>
-      <h1>Hello World!</h1>     
+      <h1 className="w-full text-3xl text-center font-bold hover:underline">Hello World!</h1>
+      <div className="flex justify-center">
+        <Button shadow auto className="text-center px-10">
+          Click me
+        </Button>
+      </div>
     </>
   );
 }
